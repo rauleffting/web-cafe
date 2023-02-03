114d6e6f020feedf3178bef5690adefd6f86f623
unknown <rauleffting94@gmail.com>
2023-02-03 20:24:08 -0300

    add panel with titles, subtitles and canva

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,44 @@
 import { HomeContainer } from './styles'
+import coffees from '../../assets/coffees.svg'
+import cart from '../../assets/cart.svg'
+import clock from '../../assets/clock.svg'
+import box from '../../assets/box.svg'
+import cup from '../../assets/cup.svg'
 
 export function Home() {
   return (
     <HomeContainer>
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>Find the perfect coffee for any time of day</h1>
+        <p>With Web Cafe, you get your coffee wherever you are, anytime</p>
+        <div className="content-wrapper">
+          <div>
+            <div className="content-wrapper2">
+              <img src={cart} alt="cart icon" />
+              <span>Simple and secure purchase</span>
+            </div>
+            <div className="content-wrapper2">
+              <img src={clock} alt="clock icon" />
+              <span>Fast and tracked delivery</span>
+            </div>
+            
+            
+          </div>
+          <div>
+            <div className="content-wrapper2">
+              <img src={box} alt="box icon" />
+              <span>Packaging keeps the coffee intact</span>
+            </div>
+            <div className="content-wrapper2">
+              <img src={cup} alt="cup icon" />
+              <span>Coffee arrives fresh to you</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={coffees} alt="three coffee in the cup and coffee seeds" />
+      </div>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,55 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  margin-top: 5.875rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1 {
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 3rem;
+    line-height: 130%;
+
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 130%;
+    margin-bottom: 4.125rem;
+  }
+
+  .content-wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 2.5rem;
+  }
+
+  .content-wrapper2 {
+    display: flex;
+    flex-direction: row;
+    gap: 0.75rem;
+
+    align-items: center;
+    margin-bottom: 1.25rem;
+  }
+
+  span {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 130%;
+
+    color: ${(props) => props.theme['gray-700']};
+  }
+`
